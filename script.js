6f20af6aaf60b2f4513b38e7bf51815ad1848f58
Sheryl Ann Navarro <90890463+SherylAnnNavz@users.noreply.github.com>
2024-03-30 21:51:37 +0800

    Uploaded the files

---
+++ script.js
@@ -0,0 +1,10 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+const audio = document.getElementById('audio');
+
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('audio');
+  audio.play();
+});
